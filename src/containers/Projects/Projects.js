7d7bea233eba58d8089e9e1d43a81f84624313cf
Lincoln <lincoln.choy1997@gmail.com>
2020-05-28 23:32:21 +1200

    Save point

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import './Projects.scss'
+import projectsList from './projectList'
 
 class Projects extends React.Component {
   render() {
     return (
       <div className="wrapper projects-wrapper scene_element scene_element--fadeindown">
-        <h1>Projects</h1>
-        <p></p>
+        <div className="text-section">
+          <h1>Projects</h1>
+          <div className="project-card">
+            <div className="project-card-inner">
+              <div className="project-card-front">
+                {projectsList[0].name}
+              </div>
+              <div className="project-card-back">
+                {projectsList[1].name}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
---
+++ src/containers/Projects/projectList.js
@@ -0,0 +1,69 @@
+export default [
+  {
+    name: "Chime",
+    description: 
+    `A messenging application developed in Angular, React and React-Native (currently there is a different implementation in each of these frameworks).
+    I also built a back-end server for this.
+    `,
+    features: ["Developed across 3 frameworks."],
+    tools: ["JavaScript/TypeScript","React, Redux", "Angular, NGRX", "React-Native", "NodeJS", "Express", "PostGreSQL"],
+    links: [
+        {
+            url: "https://github.com/lincolnChoy/Chime-Front-End",
+            name: "GitHub - React"
+        },
+        {
+            url: "https://github.com/lincolnChoy/Chime-Angular",
+            name: "GitHub - Angular"
+        },
+        {
+            url: "https://github.com/lincolnChoy/Chime",
+            name: "GitHub - React Native"
+        },
+        {
+            url: "https://github.com/lincolnChoy/Chime-API",
+            name: "GitHub - Node back-end"
+        },
+        {
+            url: "https://chimeapp.netlify.com/",
+            name: "Visit site"
+        }
+    ],
+    status: "In progress",
+    screenshots: [],
+    brief: "A full-stack messenger application for desktop and mobile."
+  },
+  {
+    name: "Pacamana Jones",
+    description: 
+    `University project. A parody of a timeless classic, Pacman. Developed in Java using the JavaFX library with fellow student Henry Shen.
+    This version features an Indiana-Jones themed storyline in which the player must collect artifacts and avoid the ghosts in the maze. 
+    `,
+    features: ["Single-player", "Multi-player", "Desktop application"],
+    tools : ["Java", "JavaFX", "Audacity", "Photoshop"],
+    links: [{
+        url: "https://github.com/lincolnChoy/Pacamana-Jones",
+        name: "GitHub"
+    }],
+    status: "Complete",
+    screenshots: ["https://i.imgur.com/g2sZMSI.png"],
+    brief: "A parody of the timeless arcade game, Pacman."
+    },
+    {
+        name: "SwimmyFishy",
+        description: "A spin-off on the classic FlappyBird game developed on Android. Developed with fellow student Henry Shen.",
+        features: ["Developed in Java.", "All visual assets were self made."],
+        tools: [
+            "Android Studio",
+            "Java",
+            "Photoshop"
+        ],
+        links: [{
+            url: "https://github.com/lincolnChoy/SwimmyFishy",
+            name: "GitHub"
+        }],
+        status: "Complete",
+        screenshots: ["https://i.imgur.com/ecGV17i.png"],
+        brief: "A spin-off on the classic FlappyBird game developed on Android."
+    },
+]
